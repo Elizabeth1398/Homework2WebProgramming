3c2a519fc1a95a99e5755a48354c29404b10f31c
Elizabeth1398 <54599736+Elizabeth1398@users.noreply.github.com>
2021-04-05 22:54:30 -0600

    Add files via upload

---
+++ htdocs/routes.php
@@ -0,0 +1,8 @@
+<?php
+  Route::get('/', function () { return view('Index'); });
+  Route::get('Book1', function () { return view('Book1'); });
+  Route::get('Book2', function () { return view('Book2'); });
+  Route::get('Book3', function () { return view('Book3'); });
+  Route::get('Book4', function () { return view('Book4'); });
+  Route::dispatch();
+?>
